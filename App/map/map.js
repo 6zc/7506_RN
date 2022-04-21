a5b602f1a14a36a0c90c61383884f095cabf452c
zichang.liu <>
2022-04-21 11:58:46 +0800

    适配安卓

--- App/map/map.js
+++ App/map/map.js
@@ -3,11 +3,10 @@ import { StyleSheet, View, Text, Dimensions, Alert } from 'react-native';
 import MapView, {
   Marker,
   Callout,
-  CalloutSubview,
   ProviderPropType,
 } from 'react-native-maps';
 import CustomCallout from './custom_callout';
-import Geolocation from '@react-native-community/geolocation';
+import Geolocation from 'react-native-geolocation-service';
 import Cal from './calculate_color';
 
 const Map = props => {
@@ -39,11 +38,6 @@ const Map = props => {
       setLatitude(info.coords.latitude)
       setLongitude(info.coords.longitude)
     });
-    console.log(curStation)
-    if(refs[curStation]){
-      console.log(refs[curStation])
-      // refs[curStation].current.showCallout()
-    }
   });
 
   const onCallout = () => {
@@ -55,7 +49,7 @@ const Map = props => {
       <MapView
         provider={props.provider}
         style={styles.map}
-        // showsUserLocation={true}
+        showsUserLocation={true}
         region={region}
         minZoomLevel={10}
         maxZoomLevel={15}
@@ -69,15 +63,14 @@ const Map = props => {
               onSelect={()=>onCallout()}
               onDeselect={()=>onCallout()}
               coordinate={marker.coordinate}
-              calloutOffset={{ x: 0, y: 60 }}
-              calloutAnchor={{ x: 0.5, y: 1 }}
+              calloutOffset={{ x: 0, y: 30 }}
+              calloutAnchor={{ x: 0.5, y: 0.7 }}
             >
-              <View style={[styles.customMarker, {backgroundColor:Cal(marker.temp), opacity: showMarkers? 1:0}]}>
-                <View style={styles.textWrapper}>
+              <View style={styles.customMarker}>
+                <View style={[styles.textWrapper, {backgroundColor:Cal(marker.temp)}, {opacity: showMarkers? 1:0}]}>
                   <Text>{marker.temp}</Text>
                   <Text style={styles.celsius}>{'°C'}</Text>
                 </View>
-                <View style={[styles.arrow, {backgroundColor:Cal(marker.temp)}]}></View>
               </View>
               <Callout
                 alphaHitTest
@@ -111,45 +104,27 @@ Map.propTypes = {
 
 const styles = StyleSheet.create({
   textWrapper:{
-    flexDirection:'row'
+    flexDirection: 'row',
+    alignSelf: 'flex-start',
+    alignItems:'center',
+    paddingHorizontal: 5,
+    paddingVertical: 0,
+    borderRadius: 6,
+    borderColor: '#eeeeee',
+    borderWidth: 0.5,
+    zIndex:2,
   },
   celsius:{
     fontSize:12
   },
-  arrow: {
-    width:8,
-    height:8,
-    transform:[{rotate:'45deg'}],
-    position:'absolute',
-    // left:"33%",
-    top:"94%",
-    borderRadius:1,
-    borderBottomColor:"gray",
-    borderBottomWidth:1.5,
-    borderRightColor:"gray",
-    borderRightWidth:1.5,
-    alignSelf: 'center',
-
-  },
   customMarker: {
     flexDirection: 'column',
     alignSelf: 'flex-start',
-    padding:1,
-    borderRadius:2,
-    borderColor:"gray",
-    borderWidth:1,
-    elevation:1,
-    shadowColor:"black",
-    shadowOffset:{width:0,height:0},
-    shadowOpacity: 1,
-    shadowRadius: 0.7,
+    alignItems:'center',
   },
   customView: {
-    width: 140,
-    height: 140,
-  },
-  plainView: {
-    width: 60,
+    // width: 140,
+    // height: 140,
   },
   container: {
     ...StyleSheet.absoluteFillObject,
@@ -159,38 +134,6 @@ const styles = StyleSheet.create({
   map: {
     ...StyleSheet.absoluteFillObject,
   },
-  // bubble: {
-  //   flex: 1,
-  //   backgroundColor: 'rgba(255,255,255,0.7)',
-  //   paddingHorizontal: 18,
-  //   paddingVertical: 12,
-  //   borderRadius: 20,
-  // },
-  // latlng: {
-  //   width: 200,
-  //   alignItems: 'stretch',
-  // },
-  // button: {
-  //   width: 80,
-  //   paddingHorizontal: 12,
-  //   alignItems: 'center',
-  //   marginHorizontal: 10,
-  // },
-  // buttonContainer: {
-  //   flexDirection: 'row',
-  //   marginVertical: 20,
-  //   backgroundColor: 'transparent',
-  // },
-  // calloutButton: {
-  //   width: 'auto',
-  //   backgroundColor: 'rgba(255,255,255,0.7)',
-  //   paddingHorizontal: 6,
-  //   paddingVertical: 6,
-  //   borderRadius: 12,
-  //   alignItems: 'center',
-  //   marginHorizontal: 10,
-  //   marginVertical: 10,
-  // },
 });
 
 export default Map;
